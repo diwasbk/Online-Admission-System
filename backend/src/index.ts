8adf1f83a5bed304e00c9b4ae362c97eb09ab5c7
diwasbk <diwasbk903@gmail.com>
2026-03-08 18:21:11 +0545

    feat(server): setup Express app and start server with DB connection

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import app from "./app"
+import { PORT } from "./config/config"
+import connectDB from "./db/db";
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`✅ Server is running at the port ${PORT}.`);
+        });
+        
+    } catch (err: any) {
+        console.log("❌ Failed to start server: ", err.message);
+        process.exit(1);
+    };
+};
+
+startServer();
