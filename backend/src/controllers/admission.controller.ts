e2be0897b8379001b9a83b81b306687c4ba90b1b
diwasbk <diwasbk903@gmail.com>
2026-03-12 20:21:17 +0545

    feat(admission-controller): update admission details using admission ID

--- backend/src/controllers/admission.controller.ts
+++ backend/src/controllers/admission.controller.ts
@@ -99,6 +99,62 @@ class AdmissionController {
             });
         };
     };
+
+    // Update Admission Detail by Admission ID
+    updateAdmissionDetailByAdmissionID = async (req: Request, res: Response) => {
+        try {
+            const admissionExist = await admissionModel.findOne({ _id: req.params.admissionId });
+
+            if (!admissionExist) {
+                return res.status(404).send({
+                    message: "Admission not found!",
+                    result: false
+                });
+            };
+
+            const { first_name, last_name, dob, gender, email, phone, address, school_name, passed_year, gpa, applied_for } = req.body;
+
+            let calculatedAge;
+
+            if (dob) {
+                calculatedAge = ageCalculator(dob);
+            };
+
+            const result = await admissionModel.findOneAndUpdate(
+                { _id: req.params.admissionId },
+                {
+                    $set: {
+                        first_name: first_name,
+                        last_name: last_name,
+                        dob: dob,
+                        age: calculatedAge,
+                        gender: gender,
+                        email: email,
+                        phone: phone,
+                        address: address,
+                        school_name: school_name,
+                        passed_year: passed_year,
+                        gpa: gpa,
+                        applied_for: applied_for
+                    }
+                }, { new: true }
+            );
+
+            res.status(200).send({
+                message: "Admission detail updated successfully!",
+                result: result,
+                success: true
+            });
+
+        } catch (err: any) {
+            console.log(err);
+
+            res.status(500).send({
+                message: err.response?.message ? `Internal server error: ${err.message}` : "Internal server error!",
+                success: false
+            });
+        };
+    };
 };
 
 export default AdmissionController;
